1e2fddb61528200f0ba95acecae5dbe9c9337363
Theofilus Handoyo <realth99@gmail.com>
2026-03-08 19:38:08 +0700

    feat: Implement product CRUD API endpoints including creation, retrieval, update, and deletion.

--- src/modules/products/route.ts
+++ src/modules/products/route.ts
@@ -12,6 +12,8 @@ function generateSlug(body: { slug?: string; name: string }) {
   body.slug = slugify(body.slug ? body.slug : body.name, {
     lower: true,
     strict: true,
+    trim: true,
+    remove: /[*+~.()"!:@]/g,
   });
   return body.slug;
 }
